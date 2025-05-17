6fbf8ca47de30c023e371f9486377537d180ef98
gupi debnath <iamgupidebntah@gmail.com>
2025-05-17 11:56:20 +0530

    Add missing home.css file

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import RightBar from "../../components/RightBar/RightBar";
-
 import "./home.css";
 
 export default function Home() {
